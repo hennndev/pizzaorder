5934ab8b22818dce34d517f49546fe0a862000ac
hennndev <hendraputra230897@gmail.com>
2022-01-26 00:27:43 +0700

    bug-fixed

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -2,7 +2,15 @@
 import clientPromise from "lib/mongodb";
 
 export default async function handler(req, res) {
+    
+    if(req.method === 'GET') {
+        const client = await clientPromise
+        const db = client.db()
 
+        const data = await db.collection('products').find({}).toArray()
+        res.status(200).json({message: 'Sukses mengambil products', data})
+    }
+    
     if(req.method === 'POST') {
         const client = await clientPromise
         const db = client.db()
@@ -11,16 +19,8 @@ export default async function handler(req, res) {
             diunggahPada: new Date(),
             status: 'tersedia'
         }
-
         await db.collection('products').insertOne(newProduct)
         res.status(201).json({message: 'Sukses menambahkan produk baru'})
     }
 
-    if(req.method === 'GET') {
-        const client = await clientPromise
-        const db = client.db()
-
-        const data = await db.collection('products').find({}).toArray()
-        res.status(200).json({message: 'Sukses mengambil products', data})
-    }
 }
