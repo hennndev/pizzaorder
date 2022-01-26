1609183c9f2f06f83afb833fda79a430d74a50c5
hennndev <hendraputra230897@gmail.com>
2022-01-26 09:52:32 +0700

    fix-bug

--- components/Cart/AddNote.js
+++ components/Cart/AddNote.js
@@ -5,7 +5,8 @@ const AddNote = ({handleAddNote, isNote, setIsNote}) => {
     
     const [value, setValue] = useState('')
 
-    const handleConfirm = () => {
+    const handleConfirm = (e) => {
+        e.preventDefault()
         setIsNote(false)
         handleAddNote(value.trim().replaceAll('/n' , ''))
         setValue('')
@@ -28,19 +29,21 @@ const AddNote = ({handleAddNote, isNote, setIsNote}) => {
             <p className='text-gray-500 mb-3 text-sm'>
                 *Kamu bisa request extra topping ( beef, chicken, salad, egg, mushroom, keju, sosis ) <br /> *Akan ada tambahan harga
             </p>
-            <textarea 
-                rows="10" 
-                className='border border-gray-200 w-full shadow-md rounded outline-none text-gray-600 text-sm p-3' 
-                value={value} 
-                onChange={(e) => setValue(e.target.value)}></textarea>
+            <form onSubmit={handleConfirm}>
+                <textarea 
+                    rows="10" 
+                    className='border border-gray-200 w-full shadow-md rounded outline-none text-gray-600 text-sm p-3' 
+                    value={value} 
+                    onChange={(e) => setValue(e.target.value)}></textarea>
 
-            <div className="flex-between space-x-3 mt-3">
-                <button className='btn bg-gray-600 hover:bg-gray-700 text-sm' onClick={() => setIsNote(null)}>Cancel</button>
-                <div className="flex items-center space-x-3">
-                    <button className='bg-red-500 hover:bg-red-600 text-sm btn' onClick={handleClear}>Clear</button>
-                    <button className='btn text-sm' onClick={handleConfirm}>Confirm</button>
+                <div className="flex-between space-x-3 mt-3">
+                    <button className='btn bg-gray-600 hover:bg-gray-700 text-sm' onClick={() => setIsNote(null)} type='button'>Cancel</button>
+                    <div className="flex items-center space-x-3">
+                        <button className='bg-red-500 hover:bg-red-600 text-sm btn' onClick={handleClear} type='button'>Clear</button>
+                        <button className='btn text-sm' type='submit'>Confirm</button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </Modal>
     )
 }
